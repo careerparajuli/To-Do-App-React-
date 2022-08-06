99dd4d622a18ad53d9a60184a1b09d5a093d2914
careerparajuli <career.parajuli@gmail.com>
2022-08-06 16:05:29 -0600

    2nd Phase-- Extracting from notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Heading from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from './notes';
+
+
+function createCard(card){
+    return (<Note 
+    key = {card.key}
+        title= {card.title}
+        content = {card.content}
+
+    />);
+}
 
 function App(){
     return <div>
     <Heading />
-    <Note />
+    {notes.map(createCard)}
     <Footer />
     </div>
 }
