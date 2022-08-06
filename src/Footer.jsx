d83fe48305753313bdd2932dbf88b5006dd16938
careerparajuli <career.parajuli@gmail.com>
2022-08-06 08:32:34 -0600

    Project Started

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+function Footer(){
+
+    const date = new Date().getFullYear();
+    return (<footer> <p> copyright © {date}</p></footer>)
+}
+
+export default Footer;
